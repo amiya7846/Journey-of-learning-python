44acf7fa4434d0cba0ba15af9e2fbffb4d8f776e
Amiya Ranjan Swain <swain.amiya24@gmail.com>
2025-09-05 19:47:10 +0530

    Added Chapter 1

---
+++ Chapter 3/01_String.py
@@ -0,0 +1,49 @@
+# String is immutable 
+# Creating strings
+name = "Amiya"
+greeting = 'Hello, World!'
+
+# String concatenation
+full_greeting = greeting + " My name is " + name
+
+# String length
+length = len(full_greeting)
+
+# Accessing characters
+first_char = name[0]      # 'A'
+last_char = name[-1]      # 'a'
+
+# Slicing strings
+first_three = name[:3]    # 'Ami'
+
+# Changing case
+upper_name = name.upper() # 'AMIYA'
+lower_greeting = greeting.lower() # 'hello, world!'
+
+# Finding substrings
+index = greeting.find('World')    # 7
+
+# Replacing substrings
+new_greeting = greeting.replace('World', 'Python') # 'Hello, Python!'
+
+# Splitting strings
+words = greeting.split(', ')     # ['Hello', 'World!']
+
+# Stripping whitespace
+spaced = "   hello   "
+stripped = spaced.strip()        # 'hello'
+
+# Print examples
+print(full_greeting)
+print("Length:", length)
+print("First char:", first_char)
+print("Last char:", last_char)
+print("First three:", first_three)
+print("Uppercase:", upper_name)
+print("Lowercase:", lower_greeting)
+print("Index of 'World':", index)
+print("Replaced:", new_greeting)
+print("Words:", words)
+print("Stripped:", stripped)
+
+
